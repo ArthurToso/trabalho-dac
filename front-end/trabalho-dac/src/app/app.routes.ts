e06da56c775fbd742490c40a96d60d79654d9f58
Alires Rosa <alires.cardoso@ufpr.br>
2025-04-02 23:41:28 -0300

    feat(menu-func/R11): adicionar tela inicial do funcionário com tabela de voos e configuração de rota

--- front-end/trabalho-dac/src/app/app.routes.ts
+++ front-end/trabalho-dac/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { MenuClienteComponent } from './pages/menu-cliente/menu-cliente.component';
+import { MenuFuncionarioComponent } from './pages/menu-funcionario/menu-funcionario.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     path: 'signup',
     component: CadastroComponent
   },
-  { path: 'menu-cliente', component: MenuClienteComponent }
+  { path: 'menu-cliente', component: MenuClienteComponent },
+  {
+    path: 'menu-funcionario',
+    component: MenuFuncionarioComponent
+  }
 ];
