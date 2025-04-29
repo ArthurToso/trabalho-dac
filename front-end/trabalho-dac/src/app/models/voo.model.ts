331113d011e67e5780b86cdd43c4790da2b40c81
Alires Rosa <alires.cardoso@ufpr.br>
2025-04-29 13:45:05 -0300

    feat(R12): implementado modal css e html falta logica

--- front-end/trabalho-dac/src/app/models/voo.model.ts
+++ front-end/trabalho-dac/src/app/models/voo.model.ts
@@ -1,12 +1,14 @@
 export class Voo {
+  
   constructor(
-    public codigo_voo?: string,
-    public data_hora?: string, 
-    public aeroporto_origem?: string,
-    public aeroporto_destino?: string,
+    public id?: string,
+    public dataHora?: string, 
+    public origem?: string,
+    public destino?: string,
     public valor_passagem?: number,
     public poltronas_total?: number,
     public poltronas_ocupadas?: number,
-    public estado_voo?: string
+    public status?: string,
+    public codigo_voo?: number
   ) {}
 }
--- front-end/trabalho-dac/src/app/pages/funcionario/menu-funcionario/menu-funcionario.component.ts
+++ front-end/trabalho-dac/src/app/pages/funcionario/menu-funcionario/menu-funcionario.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReservaService } from '../../../services/reserva.service';
-import { HeaderFuncionarioComponent } from "../header-funcionario/header-funcionario.component";
-
+import { HeaderFuncionarioComponent } from '../header-funcionario/header-funcionario.component';
+import { VooService } from '../../../services/voo.service';
+import { Voo } from '../../../models/voo.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalVerificarReservaComponent } from '../modal-verificar-reserva/modal-verificar-reserva.component';
 
 @Component({
   selector: 'app-menu-funcionario',
@@ -10,21 +13,33 @@ import { HeaderFuncionarioComponent } from "../header-funcionario/header-funcion
   templateUrl: './menu-funcionario.component.html',
   styleUrl: './menu-funcionario.component.css',
 })
-export class MenuFuncionarioComponent {
-  isMenuOpen: boolean = false; // Estado do menu responsiv
-  reservas: any[] = []; // Declaração da propriedade 'reservas'
+export class MenuFuncionarioComponent implements OnInit {
+ 
+  voos: Voo[] = [];
 
-  constructor(
-    private serviceReserva : ReservaService
-  ) {
-    // Inicialização do array 'reservas'
-    this.serviceReserva.getReservas().subscribe(reservas =>{
-      this.reservas = reservas
+  constructor(private vooServiece : VooService,
+    private modalService: NgbModal
+  ) {}
+  
+  ngOnInit(): void {
+    // array 'reservas'
+    this.vooServiece.getVoos().subscribe((voos) => {
+      this.voos = voos;
     });
+  }
+
+  confirmar(codigo_voo : number): void {
+    console.log('codigo: ' +codigo_voo);
+    this.abrirModal(codigo_voo, ModalVerificarReservaComponent)
     
   }
 
-  toggleMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen; // Alterna o estado do menu
+  cancelar(id : number) : void{
+
+  }
+
+  abrirModal(codigo_voo : number, componentModal: any) {
+    const modalRef = this.modalService.open(componentModal);
+     modalRef.componentInstance.codigo = codigo_voo;
   }
 }
