f322e75294a9c6d7ccc0439faa4bf069220d7728
Alires Rosa <alires.cardoso@ufpr.br>
2025-04-29 11:14:17 -0300

    importacoes

--- front-end/trabalho-dac/src/main.ts
+++ front-end/trabalho-dac/src/main.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http'; // Importação do HttpClientModule
 import { RouterModule } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 enableProdMode();
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
-    importProvidersFrom(RouterModule.forRoot(routes))
+    importProvidersFrom(RouterModule.forRoot(routes)),
+    provideAnimations(),   // necessário para Toastr
+    provideToastr(),
   ]
 }).catch(err => console.error(err));
