662ea16a391c6315442ce89dd764ebc5a436b0c8
xXPedrowXx <phlopes646@gmail.com>
2025-05-14 21:04:44 -0300

    (feat) BACK-END inicio implementação loginconsumer

---
+++ back-end/login/consumidor/src/main/java/exemplo1/microsservicos/consumidor/dto/UsuarioDTO.java
@@ -0,0 +1,97 @@
+package exemplo1.microsservicos.consumidor.dto;
+
+public class UsuarioDTO {
+
+    private String id;
+    private String cpf;
+    private String nome;
+    private String email;
+    private String cep;
+    private String rua;
+    private String numero;
+    private String cidade;
+    private String estado;
+    private int milhas;
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public int getMilhas() {
+        return milhas;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setMilhas(int milhas) {
+        this.milhas = milhas;
+    }
+}
